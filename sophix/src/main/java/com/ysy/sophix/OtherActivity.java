1e71f85535fbb45d9250b26d954594b66a9d116c
Shengyu Yao <admin@ysy950803.cn>
2018-04-22 22:57:10 +0800

    Test 8

--- sophix/src/main/java/com/ysy/sophix/OtherActivity.java
+++ sophix/src/main/java/com/ysy/sophix/OtherActivity.java
@@ -8,5 +8,6 @@ public class OtherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_other);
     }
 }
